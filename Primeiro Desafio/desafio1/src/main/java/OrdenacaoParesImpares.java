18f7a52198caa327141a364a597e59395002e395
guscaxile <gustavocaxile@outlook.com>
2023-07-13 10:13:55 -0300

    Desafio 1 concluído

---
+++ Primeiro Desafio/desafio1/src/main/java/OrdenacaoParesImpares.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class OrdenacaoParesImpares {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> pares = new ArrayList<>();
+        List<Integer> impares = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int valor = scanner.nextInt();
+            if (valor % 2 == 0) {
+                pares.add(valor);
+            } else {
+                impares.add(valor);
+            }
+        }
+
+        Collections.sort(pares);
+        Collections.sort(impares, Comparator.reverseOrder());
+
+        for (int valor : pares) {
+            System.out.println(valor);
+        }
+        for (int valor : impares) {
+            System.out.println(valor);
+        }
+    }
+}
